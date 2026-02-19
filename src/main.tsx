7240e950e6581cc3dca9d8cd247b0bba9df219ef
bipin-8 <aakash.bkt09@gmail.com>
2026-02-19 08:44:13 +0545

    Install and initialize AOS; import AOS CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function Root() {
+  useEffect(() => {
+    AOS.init({ duration: 700, once: true, mirror: false });
+  }, []);
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<Root />);
